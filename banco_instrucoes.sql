4228e1db1926d313a3ef2f785a881880e45a2b77
EnricoPieroCosta <enrico.santos@germinare.org.br>
2025-09-30 08:51:56 -0300

    primeiro commit

---
+++ banco_instrucoes.sql
@@ -0,0 +1,9 @@
+--Só usar esse comando para criação do banco baseado no template (Tabela Users e Addresses)
+
+CREATE DATABASE nodesequelize
+    WITH 
+    OWNER = seu_usuario
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'pt_BR.UTF-8'
+    LC_CTYPE = 'pt_BR.UTF-8'
+    TEMPLATE = template0;
